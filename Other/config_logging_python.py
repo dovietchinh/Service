ea615af52e70ad470df249396dc442f25692cb4f
dovietchinh <dovietchinh1998@gmail.com>
2024-12-10 00:39:31 +0800

    add ELK

--- Other/config_logging_python.py
+++ Other/config_logging_python.py
@@ -5,11 +5,11 @@ from logging.handlers import RotatingFileHandler
 def setup_logging():
     logger = logging.getLogger(__name__)
     #set level
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
 
     #Create a file handler 
     file_handler = RotatingFileHandler('app.log', maxBytes=1 * 1024 * 1024, backupCount=3)
-    file_handler.setLevel(logging.INFO)  # Set the level for the file handler
+    # file_handler.setLevel(logging.DEBUG)  # Set the level for the file handler
     
     #Create a console handler
     console_handler = logging.StreamHandler()
@@ -28,5 +28,9 @@ def setup_logging():
 
 if __name__ == "__main__":
     logger = setup_logging()
-    for i in range(100000):
-        logger.info(i)
+    for i in range(10):
+        logger.info('info')
+        logger.warning('warning')
+        logger.error('error')
+        logger.critical('critical')
+        logger.debug('debug')
